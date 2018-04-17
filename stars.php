d4e74342b656a4cc0fdc5dd5607e5e26e64f2689
kasinollakasi <38262732+kasinollakasi@users.noreply.github.com>
2018-04-17 12:58:57 +0300

    Add files via upload

---
+++ stars.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Stars</title>
+<meta charset="utf-8"/>
+</head>
+  <body>
+    <h2>Print some stars</h2>
+
+  
+
+
+  <form action="stars.php" method="post">
+    Enter a number:
+    <input type="numbers" name="stars" autofocus><br>
+    <input type="submit" value="Execute" value=""/>
+  </form>
+</body>
+</html>
